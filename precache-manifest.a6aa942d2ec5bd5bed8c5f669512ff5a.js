f1f3a9fadc7c8c1034be34a3a4ba3ef7ebe71331
AyushSehgal <ayushs25@berkeley.edu>
2020-07-05 17:59:59 +0700

    deploy homepage 2.0

---
+++ precache-manifest.a6aa942d2ec5bd5bed8c5f669512ff5a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74def2fd6ea97a4074c0cc7aafcc8514",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4506b0c055cc936b45d1",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3fb2fc765caa69fad087",
+    "url": "/static/css/main.70c490c0.chunk.css"
+  },
+  {
+    "revision": "4506b0c055cc936b45d1",
+    "url": "/static/js/2.de6620e5.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.de6620e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fb2fc765caa69fad087",
+    "url": "/static/js/main.a6e97a5c.chunk.js"
+  },
+  {
+    "revision": "4430ce0340d43e91cf04",
+    "url": "/static/js/runtime-main.1f0d4ae3.js"
+  },
+  {
+    "revision": "9d0e8d8b13e880d850e553c57882f5fd",
+    "url": "/static/media/prof.9d0e8d8b.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.294db7a6f8b96cacb58b0b3eb4462683.js"
+  "/precache-manifest.a6aa942d2ec5bd5bed8c5f669512ff5a.js"
 );
 
 self.addEventListener('message', (event) => {
