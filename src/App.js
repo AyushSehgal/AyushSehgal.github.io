43630ae238a870d720b1c8f57b4d3e0d1c25078f
Ayush Sehgal <ayushs25@berkeley.edu>
2021-01-07 21:03:59 +0700

    incorporate desktop devices and dock

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import Dock from "react-osx-dock";
+
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import CS61B from "./pages/CS61B";
+
+// import finder from "./img/finder.png";
+// import logo from "./img/61bLogo.png";
 
 function App() {
   return (
@@ -17,6 +22,13 @@ function App() {
           <Route path="/" exact component={Home} />
           <Route path="/CS61B" component={CS61B} />
         </Switch>
+        <Dock width={200} magnification={1} magnifyDirection="up">
+          {["finder", "61bLogo"].map((item, index) => (
+            <Dock.Item key={index} onClick={() => console.log(item)}>
+              <img src={`./img/${item}.png`} />
+            </Dock.Item>
+          ))}
+        </Dock>
         <Footer />
       </div>
     </Router>
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -9,42 +9,51 @@ import {
 } from "@material-ui/core";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../App.css";
+import "../window.css";
 import profi from "../img/prof.png";
 
 class Intro extends Component {
   state = {};
   render() {
     return (
-      <div
-        className="card"
-        style={{
-          background: "#282c34",
-          color: "white",
-          margin: "auto",
-          width: "90vw",
-          minWidth: "212px",
-        }}
-      >
-        <div className="card-body">
-          <div className="row">
-            <div className="col-sm-2">
-              <img className="img-fluid" src={profi} alt="Card image cap"></img>
-            </div>
-            <div className="col-sm-10">
-              <p className="cardText" style={{ fontSize: "15pt" }}>
-                Glad you stumbled upon this site. The point of this space is for
-                me to post resources I have either made and/or compiled for
-                various classes in the EECS department at UC Berkeley. Some of
-                this stuff are resources from courses when I was a student and
-                others are from my time as a member of course staff. Hope this
-                stuff helps. To get to know me better and for some of my
-                projects, click the Personal Site link in the header. Yes, I
-                have a separate website for that stuff (thanks to free online
-                website hosting!).
-              </p>
-              <p>
-                Checkout my personal github and linkedin page in the footer!
-              </p>
+      <div>
+        <div className="menu"></div>
+        <div
+          // className="card"
+          style={{
+            background: "#282c34",
+            color: "white",
+            margin: "auto",
+            width: "90vw",
+            minWidth: "212px",
+          }}
+        >
+          <div className="card-body">
+            <div className="row">
+              <div className="col-lg-2">
+                <img
+                  className="img-fluid"
+                  src={profi}
+                  alt="Card image cap"
+                  style={{ minWidth: "200px" }}
+                ></img>
+              </div>
+              <div className="col-lg-10">
+                <p className="cardText" style={{ fontSize: "15pt" }}>
+                  Glad you stumbled upon this site. The point of this space is
+                  for me to post resources I have either made and/or compiled
+                  for various classes in the EECS department at UC Berkeley.
+                  Some of this stuff are resources from courses when I was a
+                  student and others are from my time as a member of course
+                  staff. Hope this stuff helps. To get to know me better and for
+                  some of my projects, click the Personal Site link in the
+                  header. Yes, I have a separate website for that stuff (thanks
+                  to free online web hosting and some spare time in quarantine).
+                </p>
+                <p>
+                  Checkout my personal github and linkedin page in the footer!
+                </p>
+              </div>
             </div>
           </div>
         </div>
