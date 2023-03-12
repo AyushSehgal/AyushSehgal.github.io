900457600d5f7b6be65f5b1b217e4ea1ba0ddb8d
Ayush Sehgal <ayushs25@berkeley.edu>
2023-03-12 16:07:37 -0700

    clean up website directory

---
+++ scripts/hoopla.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+echo "         __"
+sleep 0.5
+echo "   _(\  |@@|"
+sleep 0.5
+echo "(__/\__ \--/ __"
+sleep 0.5
+echo "   \___|----|  |   __"
+sleep 0.5
+echo "       \ }{ /\ )_ / _\ "
+sleep 0.5
+echo "       /\__/\ \__O (__"
+sleep 0.5
+echo "      (--/\--)    \__/"
+sleep 0.5
+echo "      _)(  )(_ "
+sleep 0.5
+echo "      ---''--- "
+sleep 0.5
+echo ""
+echo ""
+echo "hoopla"
+         
+
+
+ 
+       
+       
+     
+    
+    
